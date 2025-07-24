87835bace88364d42157a511a823a061e49636c3
Jiang <108873506+zhiyangjing@users.noreply.github.com>
2025-07-25 00:53:06 +0800

    wip: plain point cloud visualize

---
+++ src/components/RenderTest.vue
@@ -0,0 +1,15 @@
+<template>
+  <canvas ref="canvas" width="800" height="600" />
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { renderPointCloud } from '@/utils/pointRender.ts'
+import { loadPlyBinary, type PlyBufferData } from '@/utils/parsePly.ts'
+
+const canvas = ref<HTMLCanvasElement | null>(null)
+onMounted(async () => {
+  const { positions, colors }: PlyBufferData = await loadPlyBinary()
+  renderPointCloud(canvas.value!, positions, colors)
+})
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,19 @@
 <!-- src/components/Viewer.vue -->
+<script setup lang="ts">
+import RenderTest from '@/components/RenderTest.vue'
+</script>
 <template>
-  <div>
-    <h2>PLY 模型信息</h2>
-    <p>总顶点数：{{ plyInfo?.vertexCount }}</p>
-    <p>示例点：</p>
-    <ul v-if="plyInfo">
-      <li v-for="(pt, idx) in plyInfo.samplePoints" :key="idx">
-        点{{ idx + 1 }}: ({{ pt.x.toFixed(2) }}, {{ pt.y.toFixed(2) }}, {{ pt.z.toFixed(2) }}) RGB:
-        ({{ pt.r }}, {{ pt.g }}, {{ pt.b }})
-      </li>
-    </ul>
+  <div class="main">
+    <RenderTest></RenderTest>
   </div>
 </template>
 
-<script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import { loadPlyFromAssets, type PlyData } from '@/utils/parsePly'
-
-const plyInfo = ref<PlyData | null>(null)
-
-onMounted(async () => {
-  plyInfo.value = await loadPlyFromAssets()
-})
-</script>
+<style>
+.main {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
